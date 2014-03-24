d7b8f6268ae477611586cb61618a6a592c664083
SilentTiger <cia.fbi.007@hotmail.com>
2014-03-24 17:19:35 +0800

    update demo page

--- 3_Code/WebServer/webServer.js
+++ 3_Code/WebServer/webServer.js
@@ -2,21 +2,23 @@ var express = require("express");
 
 var startWebServer = (function(){
 	var serverStarted = false;
-	return function(port){
-		if(serverStarted) {return;}
-		serverStarted = true;
+    return function(port) {
+        if (serverStarted) {
+            return;
+        }
+        serverStarted = true;
 
-		var app = express();
-		app.use(express.bodyParser());
-	
-		app.configure(function () {
-			app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-			app.use(express.logger());
-			app.use(express.static(__dirname + '/public'));
-		});
-	
-		app.listen(port);
-	}
+        var app = express();
+        app.use(express.bodyParser());
+
+        app.configure(function() {
+            app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+            app.use(express.logger());
+            app.use(express.static(__dirname + '/public'));
+        });
+
+        app.listen(port);
+    };
 
 })();
 

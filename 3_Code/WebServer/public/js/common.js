804f1848317c771bcf9306617a6de007cd717201
SilentTiger <cia.fbi.007@hotmail.com>
2014-03-24 22:24:58 +0800

    update demo page

---
+++ 3_Code/WebServer/public/js/common.js
@@ -0,0 +1,11 @@
+﻿function ShowMask(){
+    if ($(".common_mask").length > 0) return false;
+    $(document.body).append("<div class='common_mask'></div>");
+    setTimeout(function () { $(".common_mask").css("background-color", "rgba(0,0,0,0.4)");}, 1);
+    return true;
+}
+
+function HideMask(){
+    $(".common_mask").css("background-color", "");
+    setTimeout(function () { $(".common_mask").remove();}, 300);
+}
